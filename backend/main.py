c0f6b36da8a2e79c41f7a7ba4eee96c6e0fa82a4
Amirali Danai <danaiamirali@gmail.com>
2024-08-08 23:26:21 -0400

    Add basic route

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from algam import get_topics as get_social_media_topics
+
+app = FastAPI()
+
+@app.get("/topics/{num_topics}")
+def get_topics(num_topics: int):
+    try:
+        topics = get_social_media_topics("google", "reddit", weights=[0.8, 0.2], num_topics=num_topics)
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+    if not topics:
+        return {"error": "No topics found."}, 500
+
+    return topics, 200
+
