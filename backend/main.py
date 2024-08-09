358e958031e39e48dfc0f649a07607eb9ffcf6ca
Amirali Danai <danaiamirali@gmail.com>
2024-08-09 12:59:27 -0400

    CORS Middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from algam import get_topics as get_social_media_topics
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # URL of the frontend app (including port if applicable)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/topics/{num_topics}")
 def get_topics(num_topics: int):
